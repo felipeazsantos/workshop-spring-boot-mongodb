45526edc15a88b8d1a58019ec1e79626feea91b4
Felipe Azevedo <felipeasantos10@gmail.com>
2023-01-11 11:21:43 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/felipeazsantos/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.felipeazsantos.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felipeazsantos.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
